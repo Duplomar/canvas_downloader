ad17644a7fc2685a4b152c6aa3de2c984eef22ee
Duplomar <martin.neihoff@outlook.com>
2024-10-03 17:38:10 +0200

    Changed save location behaviour

--- main.py
+++ main.py
@@ -17,55 +17,99 @@ busy = []
 def get_location(url: str):
     return url.split("://", 1)[1]
 
+def url_to_filename(url: str):
+    location = get_location(url)
 
-async def download_page(session: aiohttp.ClientSession, url: str) -> list[str]:
+    return "".join(c for c in location if c.isalpha() or c.isdigit() or c==' ').rstrip()
+
+
+async def get_save_directory(session: aiohttp.ClientSession, url: str) -> Path | None:
     """
-    Downloads and saves the content of the url. Returns relevant urls to look up
+    Gets the save directory for the given url. Returns None if the url should not be saved
     """
+
+    save_path = None
     async with session.head(url) as response:
         content_type = response.headers.get("content-type", None)
 
-    if content_type:
-        is_html = "text/html" in content_type.split(";")
+    if not content_type:
+        return None
+    content_type = content_type.split(";")[0]
+    
+    is_html = "text/html" == content_type
 
-        location = get_location(url)
-        if location.startswith(BASE_LOCATION):
+    location = get_location(url)
+    if location.startswith(BASE_LOCATION):
 
-            save_path = (DIR_ROOT / "internal_content") / location[len(BASE_LOCATION):]
-            save_path = save_path.resolve()
-
-            # Assert save path is within the folder specified
-            if not save_path.is_relative_to(DIR_ROOT / "internal_content"):
-                return []
-
-            if is_html:
-                    async with session.get(url) as response:
-                        text = await response.text()
-                        soup = BeautifulSoup(text, 'lxml')
-                        relevant_urls = []
-                        for tag in soup.find_all():
-                            val = None
-                            if tag.name in ["audio", "embed", "img", "input", "script", "source", "track", "video"]:
-                                val = tag.get("src", None)
-                            elif tag.name in ["a", "link", "area"]:
-                                val = tag.get("href", None)
-
-                            if val:
-                                relevant_urls.append(val)
-
-                        print("downloading page", url, "to", save_path.as_posix())
-
-                        return relevant_urls
-
-            else:
-                print("downloading content", url)
-
-        elif not is_html:
+        save_path = (DIR_ROOT / "internal_content") / location[len(BASE_LOCATION):]
+        save_path = save_path.resolve()
+        # Assert save path is within the folder specified
+        if not save_path.is_relative_to(DIR_ROOT / "internal_content"):
+            return None
+        
+        if is_html:
+            save_path = save_path.parent / (save_path.name + ".html")
             
-            save_path = DIR_ROOT / "external_content"
-            print("Downloading external content", url)
+    elif not is_html:
+        if Path(url).suffix:
+            suffix = Path(url).suffix
+        else:
+            suffix = "." + content_type.split("/")[1]
 
-    return []
+        save_path = (DIR_ROOT / "external_content") / (url_to_filename(url) + suffix)
+
+    return save_path
+
+
+async def get_urls_on_page(session: aiohttp.ClientSession, text: str, url: str) -> list[tuple[str, Path]]:
+    soup = BeautifulSoup(text, 'lxml')
+    relevant_urls: set[tuple[str, Path]] = set()
+    for tag in soup.find_all():
+        found_url: str|None = None
+        if tag.name in ["audio", "embed", "img", "input", "script", "source", "track", "video"]:
+            found_url = tag.get("src", None)
+        elif tag.name in ["a", "link", "area"]:
+            found_url = tag.get("href", None)
+
+        if found_url and found_url.startswith("http"):
+            found_url = clean_url(url, found_url)
+            if found_url not in relevant_urls:
+                found_url_save_path = await get_save_directory(session, found_url)
+                if found_url_save_path:
+                    relevant_urls.add((clean_url(url, found_url), found_url_save_path))
+
+    return list(relevant_urls)
+
+
+async def download_page(session: aiohttp.ClientSession, url: str, save_path: Path) -> list[tuple[str, Path]]:
+    """
+    Downloads and saves the content of the url. Returns relevant urls to look up
+    """
+    print("downloading page", url, "to", save_path.as_posix())
+    async with session.get(url) as response:
+        content_type = response.headers.get("content-type", None)
+
+        if not content_type:
+            return []
+        content_type = content_type.split(";")[0]
+
+        if content_type == "text/html": 
+            text = await response.text()
+            relevant_urls = await get_urls_on_page(session, text, url)
+
+            for found_url, found_url_save_path in relevant_urls:
+                text = text.replace(found_url, found_url_save_path.as_posix())
+
+            with open("test.html", "w") as f:
+                f.write(text)
+            exit()
+            return relevant_urls
+
+        else:
+            print("Downloading data:", url)
+            data = await response.read()
+            return []
+
 
 
 def clean_url(ref_url: str, url: str) -> str:
@@ -80,7 +124,7 @@ async def worker(id: int, session: aiohttp.ClientSession):
     global busy
     while any(busy) or queue.qsize():
         if queue.qsize():
-            url, max_depth = await queue.get()
+            url, save_path, max_depth = await queue.get()
             location = get_location(url)
             if max_depth <=0 or location in seen:
                 continue
@@ -88,13 +132,11 @@ async def worker(id: int, session: aiohttp.ClientSession):
             seen.add(location)
 
             busy[id] = True
-            res = await download_page(session, url)
-            clean_urls = map(lambda link: clean_url(url, link), res)
-            for sub_url in clean_urls:
+            res = await download_page(session, url, save_path)
+            for sub_url, sub_url_save_path in res:
                 if sub_url.startswith("http") and max_depth - 1:
-                    await queue.put((sub_url, max_depth - 1))
+                    await queue.put((sub_url, sub_url_save_path, max_depth - 1))
                         
-            
         else:
             busy[id] = False
             await asyncio.sleep(0.5)
@@ -113,7 +155,11 @@ async def main():
 
     global BASE_LOCATION, busy
     BASE_LOCATION = get_location(args.base_url)
-    await queue.put((args.entry_url, args.max_depth))
+    await queue.put((
+        args.entry_url, 
+        (DIR_ROOT / "internal_content") / get_location(args.entry_url)[len(BASE_LOCATION):], 
+        args.max_depth
+    ))
     seen.add(args.entry_url)
     busy.extend([False] * args.n)
     
